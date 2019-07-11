ab8bbdc60ce43c713967898dc4ac6306fbe03f71
chocobo <e1b17065@st.oit.ac.jp>
2019-07-11 18:29:15 +0900

    fix: コードの位置を変更

--- game.hpp
+++ game.hpp
@@ -6,7 +6,7 @@ struct inputData{
     int op, y, x;
 };
 
-void getBeginDrawCell(int &begin_y, int &begin_x, int height, int width);
 void mainGame(const int height, const int width, const int mine_num);
 inputData inputProcess(const int height, const int width, int &cur_y, int &cur_x); // 入力処理
 void viewGame(GameData &data, const int height, const int width); // ゲーム画面描画
+void getBeginDrawCell(int &begin_y, int &begin_x, int height, int width);
